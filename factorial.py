6987c5a5531b0f02b66043da64994cea38fe3e87
Vlad Doster <mvdoster@gmail.com>
2019-12-13 21:02:50 -0500

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+# Asked at Air Worldwide
+
+# Write a function that  performs a combination
+# ex 1. factorial(4) = 24
+# ex 2. factorial(5) = 120
+# ex 3. factorial(1) = 1
+
+# Recursive approach
+def factorial(n):
+    pass
